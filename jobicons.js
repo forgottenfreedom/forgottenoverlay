0ae5d35835ed4c75b202fcd3a0b1832bec5d4051
forgottenfreedom <46841336+forgottenfreedom@users.noreply.github.com>
2020-08-31 18:23:05 +0200

    hi aho

--- jobicons.js
+++ jobicons.js
@@ -28,71 +28,88 @@ const jobs = {
     },
     Dnc: {
         jobicon: 'https://xivapi.com/cj/1/dancer.png',
-        jobcolor: 'somehex'
+        role: 'dps',
+        jobcolor: '#6ee1d6'
     },
     Brd: {
         jobicon: 'https://xivapi.com/cj/1/bard.png',
-        jobcolor: 'somehex'
+        role: 'dps',
+        jobcolor: '#6ee1d6'
     },
     Whm: {
         jobicon: 'https://xivapi.com/cj/1/whitemage.png',
-        jobcolor: 'somehex'
+        role: 'heal',
+        jobcolor: '#6ee1d6'
     },
     Ast: {
         jobicon: 'https://xivapi.com/cj/1/astrologian.png',
-        jobcolor: 'somehex'
+        role: 'heal',
+        jobcolor: '#6ee1d6'
     },
     Sch: {
         jobicon: 'https://xivapi.com/cj/1/scholar.png',
-        jobcolor: 'somehex'
+        role: 'heal',
+        jobcolor: '#6ee1d6'
     },
     War: {
         jobicon: 'https://xivapi.com/cj/1/warrior.png',
-        jobcolor: 'somehex'
+        role: 'tank',
+        jobcolor: '#6ee1d6'
     },
     Drk: {
         jobicon: 'https://xivapi.com/cj/1/darkknight.png',
-        jobcolor: 'somehex'
+        role: 'tank',
+        jobcolor: '#6ee1d6'
     },
     Gnb: {
         jobicon: 'https://xivapi.com/cj/1/gunbreaker.png',
-        jobcolor: 'somehex'
+        role: 'tank',
+        jobcolor: '#6ee1d6'
     },
     Pld: {
         jobicon: 'https://xivapi.com/cj/1/paladin.png',
-        jobcolor: 'somehex'
+        role: 'tank',
+        jobcolor: '#6ee1d6'
     },
     Blm: {
         jobicon: 'https://xivapi.com/cj/1/blackmage.png',
-        jobcolor: 'somehex'
+        role: 'dps',
+        jobcolor: '#6ee1d6'
     },
     Rdm: {
         jobicon: 'https://xivapi.com/cj/1/redmage.png',
-        jobcolor: 'somehex'
+        role: 'dps',
+        jobcolor: '#6ee1d6'
     },
     Smn: {
         jobicon: 'https://xivapi.com/cj/1/summoner.png',
-        jobcolor: 'somehex'
+        role: 'dps',
+        jobcolor: '#6ee1d6'
     },
     Nin: {
         jobicon: 'https://xivapi.com/cj/1/ninja.png',
-        jobcolor: 'somehex'
+        role: 'dps',
+        jobcolor: '#6ee1d6'
     },
     Mnk: {
         jobicon: 'https://xivapi.com/cj/1/monk.png',
-        jobcolor: 'somehex'
+        role: 'dps',
+        jobcolor: '#6ee1d6'
     },
     Sam: {
         jobicon: 'https://xivapi.com/cj/1/samurai.png',
-        jobcolor: 'somehex'
+        role: 'dps',
+        jobcolor: '#6ee1d6'
     },
     Drg: {
         jobicon: 'https://xivapi.com/cj/1/dragoon.png',
-        jobcolor: 'somehex'
+        role: 'dps',
+        jobcolor: '#6ee1d6'
     },
     Blu: {
         jobicon: 'https://xivapi.com/cj/1/bluemage.png',
-        jobcolor: 'somehex'
+        role: 'dps',
+        jobcolor: '#6ee1d6'
     }
 }
 
@@ -103,7 +120,7 @@ function pickrolecolor (role) {
     else if (role == 'tank') {
         return '#425ABD';
     }
-    else if (role == 'healer') {
+    else if (role == 'heal') {
         return '#397B18';
     }
 }
